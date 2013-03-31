4d0fa6461cace021238b7eeb9b692a68c5d1fc6c
Hyungyong Kim <yong27@gmail.com>
2013-03-31 20:31:54 +0900

    category working

--- paprikasite/paprika/urls.py
+++ paprikasite/paprika/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, url
-from paprika.views import BoardArticleList, ArticleDetail
+from paprika.views import ArticleList, ArticleDetail, CategoryList, CategoryDetail
 
 urlpatterns = patterns('',
     url(r'^(?P<board_slug>[-\w]+)/$', 
-        BoardArticleList.as_view(), 
-        name='article_list'),
+        ArticleList.as_view(), name='article_list'),
     url(r'^(?P<object_id>\d+)/(?P<slug>[-\w]+)/$', 
-        ArticleDetail.as_view(), 
-        name='article_detail'),
+        ArticleDetail.as_view(), name='article_detail'),
+
+    url(r'^(?P<board_slug>[-\w]+)/category/$', 
+        CategoryList.as_view(), name='category_list'),
+    url(r'^(?P<board_slug>[-\w]+)/category/(?P<slug>[-\w]+)/$', 
+        CategoryDetail.as_view(), name='category_detail'),
 )
--- paprikasite/paprika/views.py
+++ paprikasite/paprika/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from paprika.models import Article, Board
+from paprika.models import Article, Board, Category, Tag
 
 class ArticleList(ListView):
     model = Article
     context_object_name = 'articles'
 
-class BoardArticleList(ArticleList):
     def get_queryset(self):
         self.board = get_object_or_404(Board, slug=self.kwargs['board_slug'])
         return Article.objects.filter(board=self.board)
 
     def get_context_data(self, **kwargs):
-        context = super(BoardArticleList, self).get_context_data(**kwargs)
+        context = super(ArticleList, self).get_context_data(**kwargs)
         context['board'] = self.board
         return context
+
 
 class ArticleDetail(DetailView):
     model = Article
@@ -24,4 +24,32 @@ class ArticleDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(ArticleDetail, self).get_context_data(**kwargs)
         context['board'] = context['article'].board
+        return context
+
+
+class CategoryList(ListView):
+    model = Category
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, slug=self.kwargs['board_slug'])
+        return Category.objects.filter(article__board=self.board)
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryList, self).get_context_data(**kwargs)
+        context['board'] = self.board
+        return context
+
+
+class CategoryDetail(DetailView):
+    model = Category
+    context_object_name = 'category'
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, slug=self.kwargs['board_slug'])
+        return super(CategoryDetail, self).get_queryset()
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetail, self).get_context_data(**kwargs)
+        context['board'] = self.board
         return context
