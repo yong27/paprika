1d2dd016b0b108b3d58fc8bde66bcbe87f0ce73d
Hyungyong Kim <yong27@gmail.com>
2013-04-03 10:37:39 +0900

    django markdown filter deprecated. made by using markdown2

---
+++ paprikasite/paprika/templatetags/markups.py
@@ -0,0 +1,18 @@
+from django import template
+from django.conf import settings
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def markdown(value, arg=''):
+    try:
+        import markdown2
+    except ImportError:
+        if settings.DEBUG:
+            raise template.TemplateSyntaxError("Error in 'markdown' filter: The Python markdown2 library isn't installed.")
+        return force_text(value)
+    else:
+        return mark_safe(markdown2.markdown(force_text(value)))
+
