a22561f6ae752fcd1c9aecf24cc494acec24a480
Hyungyong Kim <yong27@gmail.com>
2013-05-22 02:31:47 +0900

    start article refactoring

--- paprikasite/paprika/tests.py
+++ paprikasite/paprika/tests.py
@@ -1,16 +1,36 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+from paprika.models import Board, Category, Article
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ArticleTest(TestCase):
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser('admin', 'admin@test.com', '1234')
+        self.board = Board.objects.create(**{
+            'type': 'blog',
+            'title': 'Test board',
+            'slug': 'test-board',
+            'markup': 'markdown',
+        })
+        self.category = Category.objects.create(**{
+            'title': 'My category',
+            'slug': 'my-category',
+        })
+        self.client = Client()
+
+    def test_create_article(self):
+        self.client.login(username=self.admin.username, password=self.admin.password)
+        response = self.client.post('/test-board/archive/create/', data={
+            'board': self.board.id,
+            'category': self.category.id,
+            'title': 'This is a test article',
+            'slug': 'test-article',
+            'content': 'hello world',
+            'tags': 'my, tags',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Article.objects.count(), 1)
+
