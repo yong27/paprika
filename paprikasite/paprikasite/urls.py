cdee83c40c79a2325735f4fc6f9f2b58a21b3ef3
Hyungyong Kim <yong27@gmail.com>
2014-03-17 17:12:22 +0900

    3rd party libs updated

--- paprikasite/paprikasite/urls.py
+++ paprikasite/paprikasite/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,12 +12,13 @@ urlpatterns = patterns('',
     # url(r'^paprikasite/', include('paprikasite.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^comments/', include('django.contrib.comments.urls')),
-
     url(r'^', include('paprika.urls')),
 )
